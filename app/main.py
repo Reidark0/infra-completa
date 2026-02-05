7f0f01dd9b9449c7354b602c885f542ef427c670
Rafael Cesar <rcesar@nuageit.com.br>
2026-02-05 11:01:02 -0300

    fix: add Env dev no main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
+import os
 from . import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    if os.getenv("FLASK_ENV") == "development":
+        app.run(host="127.0.0.1", port=5000, debug=True)
