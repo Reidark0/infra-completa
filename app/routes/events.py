9c374ade6f4146549ce8831104c95219d19bbd4c
Rafael César <rafa_anginho@hotmail.com>
2026-02-06 14:01:18 -0300

    fix: events bp typo

--- app/routes/events.py
+++ app/routes/events.py
@@ -3,21 +3,21 @@ from app.services.events_service import  create_event, list_events, update_event
 
 events_bp = Blueprint("events", __name__)
 
-@event_bp.route("/events", methods=["POST"])
+@events_bp.route("/events", methods=["POST"])
 def create():
     data = request.get_json()
     event = create_event(data["email"], data)
     return jsonify(event), 201
 
 
-@event_bp.route("/events", methods=["GET"])
+@events_bp.route("/events", methods=["GET"])
 def list_all():
     user_email = request.args.get("email")
     events = list_events(user_email)
     return jsonify(events), 200
 
 
-@event_bp.route("/events/<int:event_id>", methods=["PUT"])
+@events_bp.route("/events/<int:event_id>", methods=["PUT"])
 def update():
     data = request.get_json()
     event = update_event(event_id, data["email"], data)
@@ -28,7 +28,7 @@ def update():
     return jsonify(event), 200
 
 
-@event_bp.route("/events/<int:event_id>", methods=["DELETE"])
+@events_bp.route("/events/<int:event_id>", methods=["DELETE"])
 def delete(event_id):
     data = request.get_json()
     delete_event(event_id, data["email"])
