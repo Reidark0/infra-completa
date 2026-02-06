3e787e21c75b03c30a7f525f821930a3bddf8e2b
Rafael César <rafa_anginho@hotmail.com>
2026-02-06 15:06:20 -0300

    fix: typo no import

--- app/routes/events.py
+++ app/routes/events.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.services.events_service import  create_event, list_events, update_event, delete_event
+from app.services.events_services import  create_event, list_events, update_event, delete_event
 
 events_bp = Blueprint("events", __name__)
 
