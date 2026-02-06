64b1e531ca10b1d737c9d3a487d4a5fe7e93b4a4
Rafael César <rafa_anginho@hotmail.com>
2026-02-06 13:13:28 -0300

    test: mais uma linha comentada

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from app.routes.auth import auth_bp
-from app.routes.events import events_bp
+# from app.routes.events import events_bp
 
 def create_app():
     app = Flask(__name__)
